fd97e58b2f238202f85fea98818fa28e340b034e
Brandon Cortes Islas <itsolutions.utn@gmail.com>
2024-10-01 22:15:41 -0600

    update: Maniefest

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
  * conoce como número menor y se cambia cuando se realizan
  * modificaciones menores.
  */
-const VERSION = "1.30";
+const VERSION = "1.31";
 
 /**
  * Nombre de la carpeta de caché.
